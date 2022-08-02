f1a65435f2acb7c5c4cade80228a94a3faca35f7
Osvaldo Rodrigues De Faria Junior <osvaldo.junior2@estudante.ufla.br>
2022-08-02 17:53:48 -0300

    é não funciona ainda

--- prim.cpp
+++ prim.cpp
@@ -34,26 +34,11 @@ typedef vector<ii> vii; // vetor de pares de inteiros
 
 /* variaveis globais */
 
-// armazena a informacao se o vertice foi explorado ou nao
-vi explorado;
-
-// heap que armazena o vertices e suas prioridades a cada iteracao
-priority_queue<ii> Q;
-
-// lista de adjacencia
-vector<vii> LA;
-
-// numero de vertices
-int n;
-
-// numero de arestas
-int m;
-
-//acréscimo na AGM
-int acrescimo =999999;
+//vetor de resultados 
+vii resultados;
 
 // funcao que atualiza a fila de prioridades
-void atualizaFila(int u)
+void atualizaFila(int u, vi& explorado,vector<vii>& LA, priority_queue<ii>& Q)
 {
     explorado[u] = 1;
     for(int j = 0; j < LA[u].size(); j++)
@@ -70,10 +55,10 @@ void atualizaFila(int u)
     }
 }
 
-int prim()
+int prim(vi& explorado, int& n, int& uRemove, int& vRemove, int& wRemove, int& acrescimo,vector<vii>& LA, priority_queue<ii>& Q)
 {
     explorado.assign(n, 0);
-    atualizaFila(0);
+    atualizaFila(0,  explorado, LA, Q);
     
     // custo da AGM
     int resultado = 0;
@@ -98,10 +83,13 @@ int prim()
             if((kw - w) < acrescimo){
             
               acrescimo = (kw - w);
+              uRemove = vRemove;
+              vRemove = v;
+              wRemove = w;
             
             }
             resultado += w;         
-            atualizaFila(v); // O(logn)
+            atualizaFila(v, explorado, LA, Q); // O(logn)
         }
     }
  
@@ -110,22 +98,40 @@ int prim()
 
 int main()
 {
-    cin >> n >> m;
- 
-    for(int i = 0; i < n; i++)
+
+    int testes;
+    cin >> testes;
+    for(int i=0 ; i < testes ; i++)
     {
+        // armazena a informacao se o vertice foi explorado ou nao
+        vi explorado;
+        // heap que armazena o vertices e suas prioridades a cada iteracao
+        priority_queue<ii> Q;
+        // lista de adjacencia
+        vector<vii> LA;
+        // numero de vertices
+        int n;
+        // numero de arestas
+        int m;
+        //acréscimo na AGM
+        int acrescimo =999999;
+        int uRemove = 0, vRemove = 0,wRemove = 0;
+
+        cin >> n >> m;
+ 
+        for(int i = 0; i < n; i++)
+        {
         vii lista;
         LA.push_back(lista);
-    }
+        }
         
-    int u, v, w; // extremos das arestas, e peso de cada aresta
-    for(int i = 0; i < m; i++)
-    {
-        cin >> u  >> v  >> w;
-
+        int u, v, w; // extremos das arestas, e peso de cada aresta
+        for(int i = 0; i < m; i++)
+        {
+          cin >> u  >> v  >> w;
         u--;
         v--;
-        
+
         int contador = 0, continua = 0;
         for(vector<vii>::iterator it = LA.begin(); it != LA.end() && continua < 2; ++it)
         {
@@ -143,10 +149,40 @@ int main()
             continua++;
           }
           contador++;
-      }
+        }
+        }
+        
+        ii v3;
+
+         v3.first = prim(explorado, n, uRemove, vRemove, wRemove, acrescimo, LA, Q);
+
+        for(int j = 0; j < LA[uRemove].size(); j++)
+        {
+        ii v2 = LA[uRemove][j];
+        if(v2.first == vRemove and v2.second == wRemove) 
+        {
+            LA[uRemove].erase(LA[uRemove].begin() +j);
+        }
+        }
+
+
+        for(int j = 0; j < LA[vRemove].size(); j++)
+        {
+        ii v2 = LA[vRemove][j];
+        if(v2.first == uRemove and v2.second == wRemove) 
+        {
+        LA[vRemove].erase(LA[vRemove].begin() +j);
+        }   
+        }
+
+        v3.second = prim(explorado, n, uRemove, vRemove, wRemove, acrescimo, LA, Q);
+
+        resultados.push_back(v3);
     }
- 
-    cout << "Custo da AGM: " << prim() << endl;
-    cout << "Custo da AGM: " << prim() + acrescimo << endl;
+
+        for(int i =0 ; i < resultados.size() ; i++)
+        {
+            cout << resultados[i].first << " " << resultados[i].second << endl;
+        }
     return 0;
 }
